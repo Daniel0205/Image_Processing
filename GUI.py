34242154a28f68e75a2a07f654f5de44f1361dbd
Daniel0205 <daniel.alejandro.diaz@correounivalle.edu.co>
2019-03-24 16:14:56 -0500

    code commented and divided the logic and GUI

--- GUI.py
+++ GUI.py
@@ -1,317 +1,66 @@
 import tkinter as tk                # python 3
-import numpy as np
+import Logic as Lg
+import pydicom
 import matplotlib.pyplot as plt
-import Gaussian as filtros
+import numpy as np
+from tkinter import filedialog
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg,NavigationToolbar2Tk 
 from tkinter import font  as tkfont  # python 3
 from tkinter import ttk # python 3
 from tkinter import *
 from PIL import Image
-from math import fabs
-from tkinter import filedialog
 
-import pydicom
 
-#import cv2
-#import math
-
-LARGE_FONT = ("Verdana",12)
-
-ds = None
-columns=None
-rows=None
-data =  None
-
-matrizSobelX=[[-1,0,1],[-2,0,2],[-1,0,1]] #Gradient matrix in X
-matrizSobelY=[[-1,-2,-1],[0,0,0],[1,2,1]] #Gradient matrix in Y
-
-
-#Function that apply the convolution filter from a given convolution matrix (3x3)
-def aplicarFiltro(image,kernel,scalar):
-	copy = image.copy()
-	tamano = len(kernel)#Tamaño del Kernel
-	vecinos = int((tamano-1)/2)#numero de vecinos
-
-	for i in range(rows):
-		for j in range(columns):
-			if(i<vecinos or i>((rows-1)-vecinos) or j<vecinos or j>((columns-1)-vecinos)):
-				copy[i,j]=0
-			else:
-				px=0
-				py=0
-				aux=0.0
-				for kx in range(i-vecinos,i+vecinos+1):					
-					for ky in range(j-vecinos,j+vecinos+1):
-
-						aux = aux + (image[kx][ky]*kernel[px][py]) 
-						py += 1
-
-					#end for ky
-					px += 1
-					py = 0
-				#end for kx
-
-				aux = aux/scalar
-				aux = int(aux)
-				copy[i][j] = aux
-						
-		#end for j
-	#end for i
-
-	return copy
-###################################################################################
-
-#### Calculate Within Class Variance####
-
-def consultarInformacion():
-    inf = "INFORMACION DEL PACIENTE:\n"
-    inf += "-Patient's Name:  " + str(ds.PatientName)+"\n"
-    inf += "-Patient ID: "+ str(ds.PatientID)+"\n"
-    inf += "-Patient's Birth Date: "+ str(ds.PatientBirthDate)+"\n"
-    inf += "-Patient's Sex:  " + str(ds.PatientSex)+"\n"
-    inf += "-Patient's Age : "+ str(ds.PatientAge )+"\n"
-    inf += "-Patient's Weight: "+ str(ds.PatientWeight)+"\n"
-    inf += "-Patient Position: "+ str(ds.PatientPosition)+"\n"    
-    inf += "-Additional Patient History:  "+ str(ds.AdditionalPatientHistory)+"\n\n"
-    inf += "INFORMACION DE LA IMAGEN\n"
-    inf += "-Acquisition Date: "+ str(ds.AcquisitionDate) +"\n"
-    inf += "-Slide Name:  " + str(ds.StationName )+"\n"
-    inf += "-Study Description:  " + str(ds.StudyDescription) +"\n"
-    inf += "-MR Acquisition Type:  " + str(ds.MRAcquisitionType) +"\n"
-    inf += "-Repetition Time:  " + str(ds.RepetitionTime) +"\n"
-
-    return inf
-
-
-def llenarHistograma(imagen):
-    array = []
-
-    for i in range(np.amax(imagen)+1):
-     array.append(0)  
-
-    for i in range(rows):
-        for j in range(columns):
-            #intensidades[ds.pixel_array[i,j]-1]=intensidades[ds.pixel_array[i,j]-1]+1
-            array[imagen[i,j]]=array[imagen[i,j]]+1
-    return array
-
-
-def aplicarFiltroGau(tamano):
-    matrizGau,scalarGau = filtros.get_gaussian_filter(int((tamano-1)/2),1)
-
-    filterImageGau = data.copy() 
-    filterImageGau = aplicarFiltro(filterImageGau,matrizGau,scalarGau)
-
-    return filterImageGau
-
-
-def aplicarFiltroRay(tamano):
-    matrizRay,scalarRay = filtros.get_rayleigh_filter(int((tamano-1)/2),1)
-
-    filterImageRay = data.copy() 
-    filterImageRay = aplicarFiltro(filterImageRay,matrizRay,scalarRay)
-
-    return filterImageRay
-
-
-def ordenar(lista):
-	n = len(lista)
-
-	for i in range (n):
-		for j in range (0, n-i-1):
-			if lista[j] > lista[j+1]:
-				aux = lista[j]
-				lista[j] = lista[j+1]
-				lista[j+1] = aux
-	return lista
-
-def aplicarFiltroMe(imagen, vecinos = 1):
-    copy = imagen.copy()
-    for i in range(rows):
-        for j in range(columns):
-            if i<vecinos or i>((rows-1)-vecinos) or j<vecinos or j>((columns-1)-vecinos):
-                copy[i][j]=imagen[i][j]
-            else:
-                lista =[]*9
-                mid = 4
-
-                for x in range(i-vecinos,i+vecinos+1):
-                    for y in range(j-vecinos,j+vecinos+1):
-                        lista.append(imagen[x][y])
-                lista = ordenar(lista)
-
-                copy[i][j] = lista[4]
-
-    return copy
-
-    
-def crearMatrizGradiente(matrizX,matrizY):
-
-    image = np.zeros((rows,columns),dtype=int)
-    for i in range(rows):
-        for j in range(columns):
-            image[i,j]=fabs(matrizX[i,j])+fabs(matrizY[i,j])#int(((matrizX[i,j])**2+(matrizY[i,j])**2)**(1/2))
-
-    return image
-
-
-def aplicarSobel():
- 
-    imagenBordes = data.copy()
-    gradienteX= aplicarFiltro(imagenBordes,matrizSobelX,100)#cv2.filter2D(imagenBordes, -1,  np.asarray(matrizSobelX))
-    gradienteY= aplicarFiltro(imagenBordes,matrizSobelY,100)#cv2.filter2D(imagenBordes, -1,  np.asarray(matrizSobelY))
-
-    gradiente=crearMatrizGradiente(gradienteX,gradienteY)
-    
-    return gradiente
-
-
-def varianzaClase(intensidad,gradiente):
-    
-    total = rows*columns
-
-    sum = 0
-    for t in range (0,np.amax(gradiente)+1):
-        sum += t * intensidad[t]
-
-    sumB = 0
-    wB = 0
-    wF = 0
-
-    varMax = 0
-    threshold = 0
-
-    for t in range (0,256):
-        wB += intensidad[t]               # Weight Background
-        if (wB == 0): continue
-
-        wF = total - wB                 # Weight Foreground
-        if (wF == 0): break
-
-        sumB += float(t * intensidad[t])
-
-        mB = sumB / wB            # Mean Background
-        mF = (sum - sumB) / wF    # Mean Foreground
-
-        #Calculate Between Class Variance
-        varBetween = float(wB) * float(wF) * (mB - mF) * (mB - mF)
-
-        #Check if new maximum found
-        if (varBetween > varMax) :
-            varMax = varBetween
-            threshold = t
-    
-    return threshold
-
-  
-#Funtion that fills a binary matrix that defines the image's borders
-def definirBordes(gradiente,umbral):
-    image = np.zeros((rows,columns),dtype=int)
-    for i in range(rows):
-        for j in range(columns):
-            if (gradiente[i,j]>umbral):
-                image[i,j]=1
-            else:
-                image[i,j]=0
-    return image
-
-def aplicarOtsu(gradiente):
-    intensidadesGradiente=llenarHistograma(gradiente)
-
-    umbralBordes = varianzaClase(intensidadesGradiente,gradiente)#Otsu Thresholding
-
-    imagenBordes = definirBordes(gradiente,umbralBordes)
-
-    return imagenBordes
-
-
-def ubicarCentroides(k):
-    contador=0
-
-    centroides= data.copy()
-
-    cn = []
-
-    for i in range(k):
-        cn.append(int(255/k)*i)
-
-    
-    while(contador<2):
-        arrayCn = []
-        
-        for n in range(k):
-            arrayCn.append([])
-        for i in range(rows):
-            for j in range(columns):
-                distancias=[]
-                for n in range(k):
-                    distancias.append(fabs(cn[n]-data[i,j]))
-
-                index=distancias.index(np.amin(distancias))
-                arrayCn[index].append(data[i,j])
-
-                centroides[i,j]=index*int(255/k)
-
-        iguales=True                
-        for n in range(k):
-            if(cn[n]!=int(np.mean(arrayCn[n]))):
-                cn[n]=int(np.mean(arrayCn[n]))
-                iguales=False
-        
-        if(iguales):
-            contador+=1
-
-    return centroides
-        
+LARGE_FONT = ("Verdana",12)#font
 
 ####Configurating the GUI##################
 
+#Funtion where the image is selected
 def seleccionarImagen(parent,controller):
     global ds,columns,rows,data
 
     filename = filedialog.askopenfilename()   
 
     if(filename.find(".dcm")!=-1):
-        ds = pydicom.dcmread(filename) 
-        data =  ds.pixel_array.copy()
-        columns=  int(ds.Rows)
-        rows= int(ds.Columns)
+        Lg.ds = pydicom.dcmread(filename) 
+        Lg.data =  Lg.ds.pixel_array.copy()
+        Lg.columns=  int(Lg.ds.Rows)
+        Lg.rows= int(Lg.ds.Columns)
 
     else:
-        ds = Image.open(filename)
-        columns,rows=ds.size
-        data = np.array(ds) 
+        Lg.ds = Image.open(filename)
+        Lg.columns,rows=Lg.ds.size
+        Lg.data = np.array(Lg.ds) 
 
     page_name = ImagePage.__name__
     frame = ImagePage(parent=parent, controller=controller,filename=filename)
     controller.frames[page_name] = frame
 
-    # put all of the pages in the same location
-    # the one on the top of the stacking order
-    # will be the one that is visible.
     frame.grid(row=0, column=0, sticky="nsew")
     controller.show_frame("ImagePage")
 
+
+#Funtion that decide wich filter apply and draw the image in the GUI
 def aplicarFiltros(fig,canvas,filtro,size):
     imagenF=[]
 
     tamano=int(size[:size.index("x")])
 
     if(filtro=="Gaussiano"):
-        imagenF=aplicarFiltroGau(tamano)       
+        imagenF=Lg.aplicarFiltroGau(tamano)       
 
     elif(filtro=="Rayleigh"):
-        imagenF=aplicarFiltroRay(tamano)
+        imagenF=Lg.aplicarFiltroRay(tamano)
     
     elif (filtro=="Mediana"):
-        imagenF=aplicarFiltroMe(data.copy(),int((tamano-1)/2))
+        imagenF=Lg.aplicarFiltroMe(Lg.data.copy(),int((tamano-1)/2))
 
     elif(filtro=="Sobel"):
-        imagenF=aplicarSobel()
+        imagenF=Lg.aplicarSobel()
 
     elif(filtro=="Otsu"):
-        imagenF=aplicarSobel()
-        imagenF=aplicarOtsu(imagenF)
+        imagenF=Lg.aplicarSobel()
+        imagenF=Lg.aplicarOtsu(imagenF)
 
     
     if(len(fig.get_axes())!=1):
@@ -321,10 +70,11 @@ def aplicarFiltros(fig,canvas,filtro,size):
     canvas.draw()
 
 
+#Funtion that apply and show the segmented image
 def aplicarKMeans(fig,canvas,k):
     if(k!="Select number of centroids"):
 
-        centroides=ubicarCentroides(int(k))
+        centroides=Lg.ubicarCentroides(int(k))
 
         if(len(fig.get_axes())!=1): 
             fig.get_axes()[1].cla()
@@ -332,9 +82,10 @@ def aplicarKMeans(fig,canvas,k):
         filtroGaus.imshow(centroides)
         canvas.draw()
 
+#Funtion that build and show the histogram
 def mostrarHistograma(fig,canvas):
 
-    intensidades=llenarHistograma(data)
+    intensidades=Lg.llenarHistograma(Lg.data)
 
     if(len(fig.get_axes())!=1):
         fig.get_axes()[1].cla()
@@ -342,16 +93,12 @@ def mostrarHistograma(fig,canvas):
     histograma.plot(intensidades)
     canvas.draw()
 
-
+#Controller Frame 
 class ImageProgram(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
-
-        # the container is where we'll stack a bunch of frames
-        # on top of each other, then the one we want visible
-        # will be raised above the others
         container = ttk.Frame(self)
         container.pack(side="top", fill="both", expand=True)
         container.grid_rowconfigure(0, weight=500)
@@ -380,7 +127,7 @@ class ImageProgram(tk.Tk):
         frame = self.frames[page_name]
         frame.tkraise()
 
-
+#Main frame - start page
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -394,8 +141,7 @@ class StartPage(tk.Frame):
 
         buttonImage.pack(side=tk.TOP)
         
- #Funtion that fill the histogram's vector array
-
+ #Frame where is showed the image
 class ImagePage(tk.Frame):
     
     def __init__(self, parent, controller,filename):
@@ -414,7 +160,7 @@ class ImagePage(tk.Frame):
 
         fig = plt.Figure(figsize=(5,5), dpi=100)            
         subPlot = fig.add_subplot(121)
-        subPlot.imshow(data, cmap=plt.cm.gray)
+        subPlot.imshow(Lg.data, cmap=plt.cm.gray)
 
         canvas = FigureCanvasTkAgg(fig, self)
         canvas.draw()
@@ -437,8 +183,8 @@ class ImagePage(tk.Frame):
 
         if(filename.find(".dcm")!=-1):
             T = Text(self, width=50)
-            T.insert(END, consultarInformacion())        
-            T.config(state=DISABLED)
+            T.insert(END, Lg.consultarInformacion())        
+            T.config(state=DISABLED,font=("Verdana",9))
             T.pack(side=tk.RIGHT)
                         
         buttonBack = ttk.Button(self, text="Go to the start page", command=lambda: controller.show_frame("StartPage"))        
@@ -455,6 +201,8 @@ class ImagePage(tk.Frame):
         centroidsNum.pack(side=tk.LEFT,padx=10)      
         buttonKMeans.pack(side=tk.LEFT,padx=10)
     
+
+    """
     def asignarTamano(self,size,filtro):
 
         if(filtro=="Gaussiano"):
@@ -475,9 +223,9 @@ class ImagePage(tk.Frame):
         
         size.set("holasdf")
         
+"""
 
-        
-
+#Start the GUI
 if __name__ == "__main__":
     app = ImageProgram()
     app.mainloop()
---
+++ Logic.py
@@ -0,0 +1,256 @@
+import numpy as np
+import Gaussian as filtros
+from math import fabs
+
+#import cv2
+#import math
+
+ds = None
+columns= None
+rows= None
+data =  None
+
+matrizSobelX=[[-1,0,1],[-2,0,2],[-1,0,1]] #Gradient matrix in X
+matrizSobelY=[[-1,-2,-1],[0,0,0],[1,2,1]] #Gradient matrix in Y
+
+
+#Function that apply the convolution filter from a given convolution matrix (3x3)
+def aplicarFiltro(image,kernel,scalar):
+	copy = image.copy()
+	tamano = len(kernel)#Tamaño del Kernel
+	vecinos = int((tamano-1)/2)#numero de vecinos
+
+	for i in range(rows):
+		for j in range(columns):
+			if(i<vecinos or i>((rows-1)-vecinos) or j<vecinos or j>((columns-1)-vecinos)):
+				copy[i,j]=0
+			else:
+				px=0
+				py=0
+				aux=0.0
+				for kx in range(i-vecinos,i+vecinos+1):					
+					for ky in range(j-vecinos,j+vecinos+1):
+
+						aux = aux + (image[kx][ky]*kernel[px][py]) 
+						py += 1
+
+					#end for ky
+					px += 1
+					py = 0
+				#end for kx
+
+				aux = aux/scalar
+				aux = int(aux)
+				copy[i][j] = aux
+						
+		#end for j
+	#end for i
+
+	return copy
+###################################################################################
+
+
+#Funtion that consulte the patient's information
+def consultarInformacion():
+    inf = "INFORMACION DEL PACIENTE:\n"
+    inf += "-Patient's Name:  " + str(ds.PatientName)+"\n"
+    inf += "-Patient ID: "+ str(ds.PatientID)+"\n"
+    inf += "-Patient's Birth Date: "+ str(ds.PatientBirthDate)+"\n"
+    inf += "-Patient's Sex:  " + str(ds.PatientSex)+"\n"
+    inf += "-Patient's Age : "+ str(ds.PatientAge )+"\n"
+    inf += "-Patient's Weight: "+ str(ds.PatientWeight)+"\n"
+    inf += "-Patient Position: "+ str(ds.PatientPosition)+"\n"    
+    inf += "-Additional Patient History:  "+ str(ds.AdditionalPatientHistory)+"\n\n"
+    inf += "INFORMACION DE LA IMAGEN\n"
+    inf += "-Acquisition Date: "+ str(ds.AcquisitionDate) +"\n"
+    inf += "-Slide Name:  " + str(ds.StationName )+"\n"
+    inf += "-Study Description:  " + str(ds.StudyDescription) +"\n"
+    inf += "-MR Acquisition Type:  " + str(ds.MRAcquisitionType) +"\n"
+    inf += "-Repetition Time:  " + str(ds.RepetitionTime) +"\n"
+
+    return inf
+
+#funtion that fill the histogram with the image given 
+def llenarHistograma(imagen):
+    array = []
+
+    for i in range(np.amax(imagen)+1):
+     array.append(0)  
+
+    for i in range(rows):
+        for j in range(columns):
+            array[imagen[i,j]]=array[imagen[i,j]]+1
+    return array
+
+
+#Funtion that apply the Gaussian filter and return the image filtered
+def aplicarFiltroGau(tamano):
+    matrizGau,scalarGau = filtros.get_gaussian_filter(int((tamano-1)/2),1)
+
+    filterImageGau = data.copy() 
+    filterImageGau = aplicarFiltro(filterImageGau,matrizGau,scalarGau)
+
+    return filterImageGau
+
+#Funtion that apply the Rayleigh filter and return the image filtered
+def aplicarFiltroRay(tamano):
+    matrizRay,scalarRay = filtros.get_rayleigh_filter(int((tamano-1)/2),1)
+
+    filterImageRay = data.copy() 
+    filterImageRay = aplicarFiltro(filterImageRay,matrizRay,scalarRay)
+
+    return filterImageRay
+
+#Auxiliar funtion that sort a list given
+def ordenar(lista):
+	n = len(lista)
+
+	for i in range (n):
+		for j in range (0, n-i-1):
+			if lista[j] > lista[j+1]:
+				aux = lista[j]
+				lista[j] = lista[j+1]
+				lista[j+1] = aux
+	return lista
+
+#Funtion that apply the Median filter and return the image filtered
+def aplicarFiltroMe(imagen, vecinos = 1):
+    copy = imagen.copy()
+    for i in range(rows):
+        for j in range(columns):
+            if i<vecinos or i>((rows-1)-vecinos) or j<vecinos or j>((columns-1)-vecinos):
+                copy[i][j]=imagen[i][j]
+            else:
+                lista =[]*9
+                mid = 4
+
+                for x in range(i-vecinos,i+vecinos+1):
+                    for y in range(j-vecinos,j+vecinos+1):
+                        lista.append(imagen[x][y])
+                lista = ordenar(lista)
+
+                copy[i][j] = lista[4]
+
+    return copy
+
+#Funtion that fill the gradient matrix with the gradient X and the gradient Y
+def crearMatrizGradiente(matrizX,matrizY):
+
+    image = np.zeros((rows,columns),dtype=int)
+    for i in range(rows):
+        for j in range(columns):
+            image[i,j]=fabs(matrizX[i,j])+fabs(matrizY[i,j])
+
+    return image
+
+#Funtion that apply the Sobel filter and return the border image 
+def aplicarSobel():
+ 
+    imagenBordes = data.copy()
+    gradienteX= aplicarFiltro(imagenBordes,matrizSobelX,100)#cv2.filter2D(imagenBordes, -1,  np.asarray(matrizSobelX))
+    gradienteY= aplicarFiltro(imagenBordes,matrizSobelY,100)#cv2.filter2D(imagenBordes, -1,  np.asarray(matrizSobelY))
+
+    gradiente=crearMatrizGradiente(gradienteX,gradienteY)
+    
+    return gradiente
+
+#Funtion that find a threshold from a gradient matrix and the intensities of that matrix
+def umbral(intensidad,gradiente):
+    
+    total = rows*columns
+
+    sum = 0
+    for t in range (0,np.amax(gradiente)+1):
+        sum += t * intensidad[t]
+
+    sumB = 0
+    wB = 0
+    wF = 0
+
+    varMax = 0
+    threshold = 0
+
+    for t in range (0,256):
+        wB += intensidad[t]               # Weight Background
+        if (wB == 0): continue
+
+        wF = total - wB                 # Weight Foreground
+        if (wF == 0): break
+
+        sumB += float(t * intensidad[t])
+
+        mB = sumB / wB            # Mean Background
+        mF = (sum - sumB) / wF    # Mean Foreground
+
+        #Calculate Between Class Variance
+        varBetween = float(wB) * float(wF) * (mB - mF) * (mB - mF)
+
+        #Check if new maximum found
+        if (varBetween > varMax) :
+            varMax = varBetween
+            threshold = t
+    
+    return threshold
+
+  
+#Funtion that fills a binary matrix that defines the image's borders
+def definirBordes(gradiente,umbral):
+    image = np.zeros((rows,columns),dtype=int)
+    for i in range(rows):
+        for j in range(columns):
+            if (gradiente[i,j]>umbral):
+                image[i,j]=1
+            else:
+                image[i,j]=0
+    return image
+
+#Funtion that apply the otsu filter from a gradient matrix and return the image filtered
+def aplicarOtsu(gradiente):
+    intensidadesGradiente=llenarHistograma(gradiente)
+
+    umbralBordes = umbral(intensidadesGradiente,gradiente)#Otsu Thresholding
+
+    imagenBordes = definirBordes(gradiente,umbralBordes)
+
+    return imagenBordes
+
+#Funtion that segment the image finding the value of k centroids
+def ubicarCentroides(k):
+    contador=0
+
+    centroides= data.copy()
+
+    cn = []
+
+    for i in range(k):
+        cn.append(int(255/k)*i)
+
+    
+    while(contador<2):
+        arrayCn = []
+        
+        for n in range(k):
+            arrayCn.append([])
+        for i in range(rows):
+            for j in range(columns):
+                distancias=[]
+                for n in range(k):
+                    distancias.append(fabs(cn[n]-data[i,j]))
+
+                index=distancias.index(np.amin(distancias))
+                arrayCn[index].append(data[i,j])
+
+                centroides[i,j]=index*int(255/k)
+
+        iguales=True                
+        for n in range(k):
+            if(cn[n]!=int(np.mean(arrayCn[n]))):
+                cn[n]=int(np.mean(arrayCn[n]))
+                iguales=False
+        
+        if(iguales):
+            contador+=1
+
+    return centroides
+        
+
